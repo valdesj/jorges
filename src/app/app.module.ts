4a66bc990044aae5161a897e04b2acac59e7db2b
valdesj <valdesj271@gmail.com>
2018-10-10 15:41:35 -0700

    a lot of changes?

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule }     from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { UserComponent } from './user/user.component';
 
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -6,11 +6,23 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
+
 export class UserComponent implements OnInit {
   users: any
+  userInfo: any
+
   constructor(public httpClient: HttpClient) { }
+
   ngOnInit(){
-    let results = this.httpClient.get('/api/users').subscribe((res) => {this.users = res}
+    let results = this.httpClient.get('/api/users').subscribe((res) => {
+      this.users = res;
+    })
   }
 
+  public requestUser(data){
+    // let userInfo = this.httpClient.get('/api/users/' + userToLoad).subscribe((res) =>{
+    //   this.userInfo = res;
+    // })
+    console.log(data)
+  }
 }
