5683e0aca33a187fdd6ad2d4e23cd847d46d8a82
valdesj <valdesj271@gmail.com>
2018-10-11 16:41:06 -0700

    adding even more shit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,26 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule }     from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { MatDialogModule } from '@angular/material/dialog';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { UserComponent } from './user/user.component';
+import { UserInfoModal } from './user/user.component'
 
 @NgModule({
-  declarations: [AppComponent, DashboardComponent, UserComponent],
+  declarations: [AppComponent, DashboardComponent, UserComponent, UserInfoModal],
   imports: [
     BrowserModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatDialogModule,
+    BrowserAnimationsModule,
+    MatCardModule,
   ],
   providers: [],
+  entryComponents: [ UserInfoModal ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,28 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
-})
-
-export class UserComponent implements OnInit {
+}) export class UserComponent {
   users: any
-  userInfo: any
+  constructor(public httpClient: HttpClient, public dialog: MatDialog) { }
 
-  constructor(public httpClient: HttpClient) { }
-
-  ngOnInit(){
+  ngOnInit() {
     let results = this.httpClient.get('/api/users').subscribe((res) => {
       this.users = res;
     })
   }
 
-  public requestUser(data){
-    // let userInfo = this.httpClient.get('/api/users/' + userToLoad).subscribe((res) =>{
-    //   this.userInfo = res;
-    // })
-    console.log(data)
+  public openDialog(data): void {
+    const dialogRef = this.dialog.open(UserInfoModal, {data});
+    dialogRef.afterClosed().subscribe(result => {
+      //do something when closing
+    });
+  }
+}
+
+@Component({
+  selector: 'user-info-modal',
+  templateUrl: 'user.data.popup.html',
+}) export class UserInfoModal {
+  constructor(
+    public dialogRef: MatDialogRef<UserInfoModal>,
+    @Inject(MAT_DIALOG_DATA) public data) { }
+  
+  public onNoClick(): void {
+    this.dialogRef.close();
+    console.log(MAT_DIALOG_DATA)
   }
 }
