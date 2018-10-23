778639096752b69593a53de62d3ed95db9eb7e6a
valdesj <valdesj271@gmail.com>
2018-10-23 14:07:45 -0700

    minor cleaning up and data dumps

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,14 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-// import { Hero } from '../hero';
-// import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
-  title = 'Sortable Demo';
+  title = 'Route Editor';
 
   groups = [];
   ungroupedContacts = [];
